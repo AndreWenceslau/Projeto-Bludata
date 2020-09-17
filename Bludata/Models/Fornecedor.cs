5a7c819b83aac4024e6a682b52f7b759d96d38ee
AndreWenceslau <51447462+AndreWenceslau@users.noreply.github.com>
2020-09-17 09:53:49 -0300

    Revert "tes"
    This reverts commit 9b8719e395581bee2b5d83bd724da9edc4b327aa.

--- Bludata/Models/Fornecedor.cs
+++ Bludata/Models/Fornecedor.cs
@@ -33,8 +33,8 @@ namespace Bludata.Models
         public DateTime DataHora { get; set; }
 
         [Required]
-        public List<string> Telefone { get; set; }
-
+        public string Telefone { get; set; }
+       
         public Empresa Empresa { get; set; }
         public int EmpresaId { get; set; }
 
